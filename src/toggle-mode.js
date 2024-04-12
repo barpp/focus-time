120681aade94fb7984e0db2a22cd59bbddccf5f9
Eduardo <eduardobmkt@gmail.com>
2024-04-11 23:10:16 -0300

    First commit

---
+++ src/toggle-mode.js
@@ -0,0 +1,14 @@
+let darkMode = true
+const buttonToogle = document.getElementById("toggle-mode")
+
+buttonToogle.addEventListener("click", (event) => {
+    document.documentElement.classList.toggle("light")
+
+    const mode = darkMode ? 'light' : 'dark'
+    
+    event.currentTarget
+    .querySelector("span").textContent = `${mode} mode ativo!`
+
+    darkMode = !darkMode
+})
+
